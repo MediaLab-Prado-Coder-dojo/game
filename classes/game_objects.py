2eae673aef462f450e61c35d7e21e951ab03aabf
Antonio Anaya <anaya.4589@gmail.com>
2020-11-28 12:08:08 -0600

    Create game_objects.py
    Primer modelo de objecto serpiente y area

---
+++ classes/game_objects.py
@@ -0,0 +1,11 @@
+class Serpiente():
+    def __init__(self):
+        self.longitud = 1
+        self.cabeza = []
+        self.cuerpo = [[]]
+        self.sentido = 12
+ 
+class AreadeJuego():
+    def __init__(self):
+        x = 41
+        y = 41
